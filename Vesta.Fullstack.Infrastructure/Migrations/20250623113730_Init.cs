bfe1316acf3f879e19d6f4731ce0c9661833294e
denis <denis@PC_DEN4IK>
2025-06-23 14:48:58 +0300

    Added migrations

---
+++ Vesta.Fullstack.Infrastructure/Migrations/20250623113730_Init.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Vesta.Fullstack.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "orders",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    sender_city = table.Column<string>(type: "text", nullable: false),
+                    sender_address = table.Column<string>(type: "text", nullable: false),
+                    recipient_city = table.Column<string>(type: "text", nullable: false),
+                    recipient_address = table.Column<string>(type: "text", nullable: false),
+                    cargo_weight_kilograms = table.Column<float>(type: "real", nullable: false),
+                    cargo_collection_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orders", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "orders");
+        }
+    }
+}
